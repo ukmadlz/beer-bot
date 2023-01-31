7778e9578ac1e76268ac7233a0061ad20cecbf4c
Mike Elsmore <mike@elsmore.me>
2023-01-31 20:07:28 +0000

    Stupid build stupid requirements

--- pages/browser-source/[userId].tsx
+++ pages/browser-source/[userId].tsx
@@ -1,12 +1,17 @@
 import { useRouter } from 'next/router'
 import Script from 'next/script';
+import ComfyJS from 'comfy.js'
 import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react'
-import { collapseTextChangeRangesAcrossMultipleVersions } from 'typescript';
 
 export default function BrowserSourceForUser() {
   const router = useRouter()
   const session = useSession()
-  console.log(session)
+  let provider_token;
+  let nickname;
+  if(session) {
+    provider_token = session?.provider_token;
+    nickname = session?.user.user_metadata.nickname;
+  }
   const { userId } = router.query
   return (
     <>
@@ -15,16 +20,18 @@ export default function BrowserSourceForUser() {
         id="data-holder"
         data-user-id={userId}
         data-auth-token={process.env.NEXT_PUBLIC_TWITCH_AUTH_TOKEN}
+        // data-auth-token={provider_token}
+        data-user-name={nickname}
       ></div>
       <Script
         id="ComfyJS"
         src="https://cdn.jsdelivr.net/npm/comfy.js@latest/dist/comfy.min.js"
         onReady={() => {
           const dataHolder = document.getElementById('data-holder');
-          const { userId, authToken } = dataHolder?.dataset;
-          console.log(userId)
-          const channel = 'ukmadlz';
-          const ALLOWED_COMMANDS = {
+          const userId = String(dataHolder?.dataset?.userId);
+          const userName = String(dataHolder?.dataset?.userName);
+          const authToken = String(dataHolder?.dataset?.authToken);
+          const ALLOWED_COMMANDS: any = {
             'beer': `/api/beer/${userId}/current`,
             'beercurrent': `/api/beer/${userId}/current`,
             'beerlast': `/api/beer/${userId}/last`
@@ -35,12 +42,11 @@ export default function BrowserSourceForUser() {
                   fetch(ALLOWED_COMMANDS[command])
                   .then((response) => response.json())
                   .then((data) => {
-                      ComfyJS.Say(`${channel}'s beer was a ${data.beer.beer_name} it's a ${data.beer.beer_style} from ${data.brewery.brewery_name} in ${data.brewery.country_name}`)
-                      console.log(`${channel}'s beer was a ${data.beer.beer_name} it's a ${data.beer.beer_style} from ${data.brewery.brewery_name} in ${data.brewery.country_name}`)
+                      ComfyJS.Say(`${userName}'s beer was a ${data.beer.beer_name} it's a ${data.beer.beer_style} from ${data.brewery.brewery_name} in ${data.brewery.country_name}`, userName);
                   });
               }
           }
-          ComfyJS.Init( channel, authToken );
+          ComfyJS.Init( userName, authToken );
         }}
       >
       </Script>
